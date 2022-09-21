3b0b1fc9c564877b434290ad9b7f6295b82f2d43
LucioFurnari <luciofurnari99@gmail.com>
2022-09-20 21:14:30 -0300

    Add buttons and eventlistener

--- script.js
+++ script.js
@@ -22,21 +22,38 @@ function playRound(playerSelection, computerSelection){
     if(playerSelection == computerSelection){
         return "Empate"
     }else if(playerSelection == "rock" && computerSelection == "scissors"){
+        playerScore += 1;
         return "You Win, rock beat scissors"
     } else if (playerSelection == "paper" && computerSelection == "rock"){
+        playerScore += 1;
         return "You Win, paper beat rock"
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        playerScore += 1;
         return "You Win, scissors beat paper"
     } else {
+        computerScore += 1;
         return `You Lose, ${computerSelection} beat ${playerSelection}`
     }
+    
 }
 
 function game(){
-    for (let i = 0; i < 5; i++){
-        let playerChoise = prompt("Choise rock, paper or scissors").toLowerCase()
-        console.log(playRound(playerChoise,getComputerChoise()));  
-    }
+    // let playerChoise = prompt("Choise rock, paper or scissors").toLowerCase()
+    return (playRound(playerChoise,getComputerChoise()));  
 }
+const Buttons = document.querySelectorAll("button");
+const scoreText = document.querySelector(".score");
+let playerScore = 0;
+let computerScore = 0;
+Buttons.forEach(button => {
+    button.addEventListener("click",(e) => {
+        scoreText.textContent = playRound(e.target.id,getComputerChoise())
+        if(playerScore >= 5) {
+            console.log("Player Win");
+        } else if(computerScore >= 5){
+            console.log("Computer Win");
+        }
+    })
+})
+console.log(Buttons);
 
-game()
